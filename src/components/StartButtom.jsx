205406f5555dba5d74004c27b4525b3bf90ea9b0
Glitch-loop <renetdejesus@gmail.com>
2024-07-19 16:36:27 -0600

    Adding interaction to button in hero

--- src/components/StartButtom.jsx
+++ src/components/StartButtom.jsx
@@ -1,21 +1,29 @@
 import { faChevronRight, faAnglesRight } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-
+import { motion } from "framer-motion"
+import { Link } from "react-scroll"
 
 function StartButtom() {
   return (
-    <div 
-      className={`
-        flex justify-center items-center 
-        rounded-full bg-base-color text-background-color
-        w-10 h-10
-        md:w-16 md:h-16
-        lg:w-24 lg:h-24
-        
-        `}
-      >
-        <FontAwesomeIcon icon={faAnglesRight} />
-    </div>
+    <Link 
+    to={"section1"} 
+    smooth={true} 
+    duration={500}>
+    <motion.div 
+          whileHover={{scale: 1.1}}
+          whileTap={{ scale: 0.9 }}
+          className={`
+            flex justify-center items-center 
+            rounded-full bg-base-color text-background-color
+            w-10 h-10
+            md:w-16 md:h-16
+            lg:w-24 lg:h-24 
+            `}
+          >
+            <FontAwesomeIcon icon={faAnglesRight} />
+        </motion.div>
+    </Link>
+    
   )
 }
 
