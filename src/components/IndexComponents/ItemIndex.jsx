f519c7debdd07869a9fa3e66bbe3db7c0efc304e
Glitch-loop <renetdejesus@gmail.com>
2024-07-21 18:04:57 -0600

    Fixing index buttom (you had to click the letter to make it work)

--- src/components/IndexComponents/ItemIndex.jsx
+++ src/components/IndexComponents/ItemIndex.jsx
@@ -3,21 +3,23 @@ import { motion } from "framer-motion";
 
 function ItemIndex({to_direct, state, children}) {
   return (
-    <motion.button
-      whileHover={{scale: 1.1}}
-      whileTap={{ scale: 0.9 }}
-      className={to_direct === state ? 
-        "my-2 md:w-2/6 xl:w-1/4 cursor-pointer text-left" : 
-        "my-2 md:w-2/6 xl:w-1/4 cursor-pointer opacity-25 text-left"}
-      >
-      <Link 
-        to={to_direct} 
-        smooth={true} 
-        duration={500}>
+    <Link 
+    to={to_direct} 
+    smooth={true} 
+    duration={500}
+    className={to_direct === state ? 
+      "my-2 md:w-2/6 xl:w-1/4 cursor-pointer text-left" : 
+      "my-2 md:w-2/6 xl:w-1/4 cursor-pointer opacity-25 text-left"}
+    >
+      <motion.button
+        whileHover={{scale: 1.1}}
+        whileTap={{ scale: 0.9 }}
+        className="w-full text-start"
+        >
         <span>{children}</span>
         <div className="bg-base-color h-1 rounded-full mt-2"></div>
-      </Link>
-    </motion.button>
+      </motion.button>
+    </Link>
   )
 }
 
